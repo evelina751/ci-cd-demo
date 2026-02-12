909045f89377c2a1a73f3750499aec6e0a0fe6f4
eeremia <eeremia@adobe.com>
2026-02-12 21:52:30 +0200

    Add initial Node app

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const VERSION = "1.0.0";
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "CI/CD Demo Running 🚀",
+    version: VERSION,
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "UP" });
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
